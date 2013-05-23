5c09e97747f74faa6cf23e6ff03dfb5b0b20b24a
Daniel Moran <dmj@tid.es>
2013-05-23 17:53:02 +0200

    First draft of Puppet scripts

---
+++ deploy/deploy.pp
@@ -0,0 +1,69 @@
+# Prerequisite packages
+#################################
+package { 'git':
+ensure => installed,
+}
+
+package { 'nodejs':
+ensure => installed,
+}
+
+package { 'npm':
+ensure => installed,
+require => Package['nodejs'],
+}
+
+package { 'redis-server':
+ensure => installed,
+}
+
+notify {'Packages installed':
+require => Package['npm'],
+}
+
+# Install the appropriate version of node with n
+###################################################
+
+Exec {
+path => [
+'/usr/local/bin',
+'/opt/local/bin',
+'/usr/bin',
+'/usr/sbin',
+'/bin',
+'/sbin'],
+logoutput => true,
+}
+
+
+exec {'n':
+command => 'npm install -g n',
+unless => 'which n',
+require => Package['npm'],
+}
+
+exec {'n 0.8.14':
+unless => 'node --version |grep 0.8.14',
+require => Exec['n'],
+}
+
+notify {'Proper Node.js version installed':
+require => Exec['n 0.8.14'],
+}
+
+# Install Popbox
+#################################################
+exec {'popbox':
+command => 'git clone https://github.com/dmoranj/PopBox.git',
+unless => 'ls /home/ubuntu/PopBox',
+}
+
+exec {'dependencies':
+command => 'npm install',
+cwd     => '/home/ubuntu/PopBox' ,
+require => Exec['popbox'],
+}
+
+notify {'Popbox cloned and installed':
+require => Exec['dependencies'],
+}
