760364876b4e652098e24236bd4eacbc831802e0
Daniel Moran <dmj@tid.es>
2013-05-24 14:02:52 +0200

    Add EC2 environment checks

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -2,6 +2,11 @@
 
 . config.sh
 
+# Log an error to the console
+function error() {
+  echo -e "\n\tError: $1\n";
+}
+
 # Show the script usage
 function show_usage() {
   echo -e "Usage:"
@@ -11,7 +16,27 @@ function show_usage() {
 
 # Check if the EC2 environment is correctly set and working
 function check_ec2_environment() {
-  echo "" 
+  if [[ -z "$EC2_HOME" ]]; then
+    error "Amazon installation base variable not defined";
+    exit 1
+  fi
+
+  if [[ -z "$AWS_ACCESS_KEY" ]] || [[ -z "$AWS_SECRET_KEY" ]]; then
+    error "Amazon EC2 Access Key or secret variables not defined";
+    exit 1
+  fi
+
+  if [[ -z "$JAVA_HOME" ]]; then
+    error "JAVA_HOME variable not defined";
+    exit 1
+  fi
+
+  which ec2-describe-regions > /dev/null
+
+  if [[ $? = 1 ]]; then
+    error "EC2 Tools not installed or not found in path";
+    exit 1
+  fi
 }
 
 # Deploy an instance of the full stack. The number of Popbox Agents and 
@@ -32,7 +57,7 @@ function deploy_vm () {
 
   if [[ $AGENT_NUMBER = 0 ]]; then
   	echo "Deploying branch $GIT_BRANCH in the minimal configuration"
-	ec2-run-instances $IMAGE -t $SIZE --region $REGION --key $KEYS -g $GROUP --user-data-file $INITFILE
+	#ec2-run-instances $IMAGE -t $SIZE --region $REGION --key $KEYS -g $GROUP --user-data-file $INITFILE
   else
         echo "Deploying branch $GIT_BRANCH with $AGENT_NUMBER Agents connected to $REDIS_NUMBER Redis"
   fi
@@ -53,7 +78,6 @@ function dispatch_actions() {
     ;;
   esac
 }
-
 
 check_ec2_environment
 dispatch_actions $1 $2 $3 $4
